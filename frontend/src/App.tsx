3b4bd3b7c54a57e7243ca12ac746c859fbd516c2
ylyma <amylingyuhui@gmail.com>
2023-10-17 00:07:52 +0800

    Add routes for navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
 import TitleScreen from "./screens/TitleScreen";
-import { Box, ThemeProvider, useTheme } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
 import theme from "./assets/theme";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Box className="App">
-        <TitleScreen />
+        <Routes>
+          <Route path="/" element={<TitleScreen />} />
+        </Routes>
       </Box>
     </ThemeProvider>
   );
